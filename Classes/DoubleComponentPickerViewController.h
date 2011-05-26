0e8731c31d79798c68a86a1bffec84c6ff6b5e94
Simon Fenton <swfenton@gmail.com>
2011-05-26 21:17:13 +1000

    Added the implementation for the 3rd tab, the Double Component Picker.

--- Classes/DoubleComponentPickerViewController.h
+++ Classes/DoubleComponentPickerViewController.h
@@ -8,9 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+#define kFillingComponent 0
+#define kBreadComponent 1
 
-@interface DoubleComponentPickerViewController : UIViewController {
-
+@interface DoubleComponentPickerViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource> {
+	UIPickerView *doublePicker;
+	NSArray *fillingTypes;
+	NSArray *breadTypes;
 }
+
+@property (nonatomic, retain) IBOutlet UIPickerView *doublePicker;
+@property (nonatomic, retain) NSArray *fillingTypes;
+@property (nonatomic, retain) NSArray *breadTypes;
+
+-(IBAction)buttonPressed;
 
 @end
--- Classes/DoubleComponentPickerViewController.m
+++ Classes/DoubleComponentPickerViewController.m
@@ -11,31 +11,42 @@
 
 @implementation DoubleComponentPickerViewController
 
-// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-/*
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization.
-    }
-    return self;
-}
-*/
+@synthesize doublePicker;
+@synthesize breadTypes;
+@synthesize fillingTypes;
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
+-(IBAction)buttonPressed
+{
+	NSInteger fillingRow = [doublePicker selectedRowInComponent:kFillingComponent];
+	NSInteger breadRow = [doublePicker selectedRowInComponent:kBreadComponent];
+	
+	NSString *filling = [fillingTypes objectAtIndex:fillingRow];
+	NSString *bread = [breadTypes objectAtIndex:breadRow];
+	
+	NSString *message = [[NSString alloc] initWithFormat:@"Your %@ on %@ bread will be right up!", filling, bread];
+	
+	UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Thank you for your order"
+													message:message
+												   delegate:nil
+										  cancelButtonTitle:@"Great!"
+										  otherButtonTitles:nil];
+	
+	[alert show];
+	[alert release];
+	[message release];
 }
-*/
 
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+- (void)viewDidLoad
+{
+	NSArray *fillingArray = [[NSArray alloc] initWithObjects:@"Ham", @"Turkey", @"Roast beef", @"Vegemite", nil];
+	self.fillingTypes = fillingArray;
+	[fillingArray release];
+	
+	NSArray *breadArray = [[NSArray alloc] initWithObjects:@"Rye", @"White", @"Multi-grain", @"Gluten free", @"Wholemeal", nil];
+	self.breadTypes = breadArray;
+	[breadArray release];
 }
-*/
+
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
@@ -48,12 +59,59 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+	self.fillingTypes = nil;
+	self.breadTypes = nil;
+	self.doublePicker = nil;
 }
 
 
 - (void)dealloc {
+	[fillingTypes release];
+	[breadTypes release];
+	[doublePicker release];
     [super dealloc];
 }
 
+#pragma mark -
+#pragma	mark Picker Data Source Methods
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+	return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+	switch (component) {
+		case kBreadComponent:
+			return [self.breadTypes count];
+			break;
+		case kFillingComponent:
+			return [self.fillingTypes count];
+			break;
+
+		default:
+			return 0;
+			break;
+	}
+}
+
+#pragma mark Picker Delegate Methods
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+	switch (component) {
+		case kBreadComponent:
+			return [self.breadTypes objectAtIndex:row];
+			break;
+		case kFillingComponent:
+			return [self.fillingTypes objectAtIndex:row];
+			break;
+			
+		default:
+			return @"Unknown";
+			break;
+	}
+}
 
 @end
